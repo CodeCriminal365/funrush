d73d56dad9d3d83f0232ca1d6077cde516325e06
CodeCriminal365 <120529066+CodeCriminal365@users.noreply.github.com>
2023-02-06 15:50:30 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,32 +2,37 @@ namespace SpriteKind {
     export const GUI = SpriteKind.create()
     export const floor = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveUp(permPixels)
+})
 function moveRight (pixels: number) {
     mainPlayer.setVelocity(pixels, pixels / 5)
 }
 function moveUp (pixels2: number) {
     mainPlayer.setVelocity(pixels2 / 5, pixels2)
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveUp(25)
-})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveLeft(25)
+    moveLeft(permPixels)
 })
 function moveDown (pixels3: number) {
     mainPlayer.setVelocity(pixels3 - pixels3 * 2, pixels3 / 5)
 }
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveRight(25)
+    moveRight(permPixels)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveDown(permPixels)
 })
 function moveLeft (pixels4: number) {
     mainPlayer.setVelocity(pixels4 - pixels4 * 2, pixels4 / 5)
 }
+let permPixels = 0
 let mainPlayer: Sprite = null
 mainPlayer = sprites.create(assets.image`character`, SpriteKind.Player)
 mainPlayer.setPosition(63, 74)
 scene.setBackgroundImage(assets.image`skyBox`)
 forever(function () {
+    permPixels = 25
     mainPlayer.setStayInScreen(true)
     mainPlayer.setBounceOnWall(true)
 })
